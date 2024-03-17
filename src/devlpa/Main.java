35e3c8291441326c105900f3cf4a1c72c5a86007
adamd <adam.d.hinton@gmail.com>
2024-03-16 21:21:58 -0400

    more complicated lambda chaining

--- src/devlpa/Main.java
+++ src/devlpa/Main.java
@@ -1,5 +1,6 @@
 package devlpa;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -19,5 +20,27 @@ public class Main {
 
         uCaseLastName = uCase.compose(lastName);
         System.out.println(uCaseLastName.apply(name));
+
+        Function<String, String[]> f0 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s-> s.split(" "));
+
+        System.out.println(Arrays.toString(f0.apply(name)));
+
+
+        Function<String, String> f1 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s-> s.split(" "))
+                .andThen(s-> s[1].toUpperCase() + ", " + s[0]);
+        System.out.println(f1.apply(name));
+
+        Function<String, Integer> f2 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s-> s.split(" "))
+                .andThen(s -> String.join(", ", s))
+                .andThen(String::length);
+        System.out.println(f2.apply(name));
+
+
     }
 }
